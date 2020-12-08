2b4642997c93ec04002ffcd1c030e90e5e85e42b
DawidKolo <dawid_kolodziej@o2.pl>
2020-12-08 21:37:47 +0100

    vars

---
+++ exe.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+slownik = {'kawa': 'czarna', 'herbata':'zielona',  'cukier kostka':4, 0:'zero', 1: 'jeden'}
+slownik['herbata']
+slownik[0]
+type(slownik)
+slownik.keys()
+list(slownik.keys())
+slownik.values()
+'herbata' in slownik
+print(slownik.keys())
+print(list(slownik.keys()))
+print(slownik.values())

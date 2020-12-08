0a7d13d313adf7c1572999c5ca4af6f7fbd2ede6
DawidKolo <dawid_kolodziej@o2.pl>
2020-12-08 08:21:13 +0100

    input

---
+++ helloWorld.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+print('Jak masz na imię?')
+imie = input()
+print('Miło Cię poznać '+ imie)
+print('Ile masz lat? ')
+wiek = input()
+print('Z którego peronu można dojechać do Hogwartu? ')
+peron = input()
+print('W wieku ' + wiek + ' lat chyba nie ma sensu jechać do Hogwartu z peronu ' + peron)

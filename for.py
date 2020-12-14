2faaefd39a9539745e459fbc1dcb4c1a07b68e12
DawidKolo <dawid_kolodziej@o2.pl>
2020-12-14 20:52:36 +0100

    tasks

---
+++ for.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+for user in range(0,5):
+    name = input('jak masz na imię? ')
+    print('Czesć', name)
+
+a = range(0,10)
+print(a)
+
+
+names = ['Piotrek', 'Nina', 'Szymon', 'Justyna', 'Dawid']
+for i in range(5):
+    print('Cześć', names[i])
+for name in names:
+    print('Hej', name)
+
+
+
---
+++ zadanieFor.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+sum = 0
+
+for n in range(1,11):
+    sum = sum + n
+    print(sum)
+
+cube = 0
+
+for n in range(1,11):
+    cube = pow(n,3)
+    print(cube)
+
+
+x = str(input('podaj imiona gości '))
+for i in x.split():
+    print('Czesc', i)
+
+
+petla = int(input('Podaj liczbę naturalną '))
+
+for i in range(1, petla):
+    if i % 3 == 0 and i % 4 == 0:
+        print('hurra', i)
+    else:
+        print('*')
+    
